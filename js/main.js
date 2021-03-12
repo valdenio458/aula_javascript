1bfdd03ae8a9382f3c31a52bc574b54c36680ba9
Valdênio Martinho Pereira <valdeniomartinho@gmail.com>
2021-03-12 20:56:17 -0300

    subindo

---
+++ js/main.js
@@ -0,0 +1,68 @@
+function clicado(){
+    document.getElementById("teste").innerHTML = "Obrigado por clicar!";
+}
+/*
+function clicado(){
+    alert("Obrigado por clicar");
+}
+// Datas:
+
+var data = new Date();
+console.log(data);
+console.log(data.getMonth() + 1);
+
+// Laços de repetição:
+
+var c;
+for(c = 0; c <=5; c++){
+    console.log(c);
+}
+
+
+var count = 0;
+while(count < 5){
+    console.log(count);
+    count++;
+    
+}
+
+
+// Condicionais:
+var idade = prompt("Qual a sua idade?");
+if(idade >= 18){
+    alert("Maior de idade")
+
+    }else{
+        alert("Menor de idade")
+    }
+
+// Objeto JSON - Dicionário
+
+var frutas = [{nome: "maça", cor: "vermelha"},{nome: "pera", cor: "verde"}];
+console.log(frutas);
+console.log(frutas[1].cor);
+
+
+var frutas = {nome: "maça", cor: "vermelha"};
+console.log(frutas);
+console.log(frutas.nome);
+console.log(frutas.cor)
+
+
+var frutas = ["maça", "pera", "laranja"];
+frutas.push("uva"); // adiciona elemento ao final do array
+frutas.pop(); // remove o último elemento do array
+//console.log(frutas);
+//console.log(frutas.length); // exibe o tamanho do array
+//console.log(frutas.reverse()); // inverte os itens da lista
+console.log(frutas.toString()) // transforma o array numa lista
+console.log(frutas.join(" * "));
+//var nome = "Valdênio";
+//var idade = 62;
+//var iadade2 = 10;
+//var frase = "Japão na área"
+//alert( nome + " tem " + idade + " anos");
+//alert(idade + iadade2);
+//console.log(frase.length);
+
+*/
